2a05f55b54c0f52e5e77cc6f505569a34ae72ebe
matthieu <matthieu.roux51@orange.fr>
2026-07-16 22:43:02 +0200

    count the number of token with tiktoken

--- exercices/pdf_synth.py
+++ exercices/pdf_synth.py
@@ -1,6 +1,6 @@
 import requests
 from pypdf import PdfReader
-
+import tiktoken 
 # Download the wanted document 
 doc = requests.get("https://arxiv.org/pdf/2311.07071")
 with open("to_summarize.pdf", 'wb') as f :
@@ -21,8 +21,17 @@ for page in reader.pages:
     page.extract_text(visitor_text=visitor_body)
 text_body = "".join(parts)
 
+# Count the number of tokens the text is containing :
 
+def count_number_of_token(text)->int:
+    #get the encoding
+    encoding = tiktoken.encoding_for_model('gpt-3.5-turbo')
+    text_encode = encoding.encode(text)
+    print(text_encode[:10])
+    number_of_token = len(text_encode)
+    return number_of_token
 
-
+n_token = count_number_of_token(text_body)
+print(f'Number of token {n_token}')
 
 #if "name"==__main__:
