3f82cc1deb873beb6b486628c5251e31e9e0e243
matthieu <matthieu.roux51@orange.fr>
2026-07-16 22:25:36 +0200

    pdf text extraction

--- exercices/pdf_synth.py
+++ exercices/pdf_synth.py
@@ -1,10 +1,27 @@
 import requests
+from pypdf import PdfReader
 
 doc = requests.get("https://arxiv.org/pdf/2311.07071")
 
 with open("to_summarize.pdf", 'wb') as f :
     f.write(doc.content)
 
+reader = PdfReader("to_summarize.pdf")
+print(f"Number of pages {len(reader.pages)}")
 
+parts = []
+
+def visitor_body(text, cm, tm, font_dict, font_size):
+    y = tm[5]
+    if 50 < y < 720 or y == 0:
+        parts.append(text)
+
+for page in reader.pages:
+    
+    page.extract_text(visitor_text=visitor_body)
+
+text_body = "".join(parts)
+
+print(text_body)
 
 #if "name"==__main__:
